bca0c895b56cee537dfc8c899b97cd83ba10bcb5
Laura Rosenthal <rosentha@ut.ee>
2018-11-29 23:14:46 +0200

    javascript

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
-function vaheleht(){
-    <a href="steveJobs.html"><input class="button" type="SteveJobs" value="STEVE JOBS" name=""></a>
-    <br>
-    <a href="realityDistortionfield.html"><input class="button" type="Reality Distortion Field" value="REALITY DISTORTION FIELD" name=""></a>
+function vahelehtSteveJobs(){
+    window.location="steveJobs.html"
+}
+function vahelehtRealityDistortionField(){
+    window.location="realityDistortionfield.html"
 }
